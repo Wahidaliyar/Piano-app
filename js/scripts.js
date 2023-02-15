84861a628f3a88c123d742fe1b5a6cace87e88e4
Wahid Aliyar <aliyar.wahid21@gmail.com>
2023-02-16 02:20:33 +0430

    initial commit

---
+++ js/scripts.js
@@ -0,0 +1,39 @@
+const pianoKeys = document.querySelectorAll(".piano-keys .key");
+const volumeSlider = document.querySelector(".volume-slider input");
+const keysCheckbox = document.querySelector(".keys-checkbox input");
+
+let allKeys = [];
+let audio = new Audio("tunes/a.wav");
+
+const playTune = (key) => {
+	audio.src = `tunes/${key}.wav`;
+	audio.play();
+
+    const clickedKey = document.querySelector(`[data-key="${key}"]`);
+    clickedKey.classList.add("active");
+    setTimeout(() => {
+        clickedKey.classList.remove("active");
+    }, 150);
+};
+
+
+
+pianoKeys.forEach((key) => {
+    allKeys.push(key.dataset.key);
+	key.addEventListener("click", () => playTune(key.dataset.key));
+});
+
+const showHideKeys = () => {
+    pianoKeys.forEach(key => key.classList.toggle("hide"));
+}
+
+const volumeHandler = (e) => {
+    audio.volume = e.target.value;
+}
+
+const pressedKey = (e) => {
+    if(allKeys.includes(e.key)) playTune(e.key);
+}
+keysCheckbox.addEventListener("click", showHideKeys);
+volumeSlider.addEventListener("input", volumeHandler);
+document.addEventListener("keydown", pressedKey);
